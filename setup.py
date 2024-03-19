ffb7924a41e73470cf02e9d8ab673780a798e0f2
Basit715 <basitabass27411@gmail.com>
2024-03-19 12:45:29 +0530

    setup file created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+     name = "Book_recommendation_system",
+     version="0.0.1",
+     author = "Basit pushoo",
+     author_email="basitabass27411@gmail.com",
+     packages=find_packages()
+) 
